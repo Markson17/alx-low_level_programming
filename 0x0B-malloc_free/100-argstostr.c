378417d1c41fd57b27d1c12636f067f4a33ff46b
Markson Favour <95889759+Markson17@users.noreply.github.com>
2022-10-04 02:20:33 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,45 +1,98 @@
-#include <stdlib.h>
-#include "main.h"
-
-/**
-* *argstostr - concatenates all the arguments of the program
-* @ac: number of arguments
-* @av: array of arguments
-*
-* Return: Pointer to the new string (Success), NULL (Error)
-*/
-char *argstostr(int ac, char **av)
-{
-int i, j, k, len;
-char *str;
-
-if (ac == 0 || av == NULL)
-return (NULL);
-
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j] != '\0'; j++)
-len++;
-len++;
-}
-
-str = malloc(sizeof(char) * (len + 1));
-
-if (str == NULL)
-return (NULL);
-
-k = 0;
-
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j] != '\0'; j++)
-{
-str[k] = av[i][j];
-k++;
+u
 }
 str[k] = '\n';
-k++;
-}
 
-return (str);
+
+
+return (str)
+
+  
+Task 5
+
+#include "main.h"
+
+#include <stdlib.h>
+
+/**
+
+ * argstostr - concatenates all the arguments of a program.
+
+ * @ac: argument count.
+
+ * @av: argument vector.
+
+ *
+
+ * Return: pointer of an array of char
+
+ */
+
+char *argstostr(int ac, char **av)
+
+{
+
+	char *aout;
+
+	int c, i, j, ia;
+
+	if (ac == 0)
+
+		return (NULL);
+
+	for (c = i = 0; i < ac; i++)
+
+         {
+
+		if (av[i] == NULL)
+
+			return (NULL);
+
+		for (j = 0; av[i][j] != '\0'; j++)
+
+			c++;
+
+		c++;
+
+	}
+
+		aout = malloc((c + 1) * sizeof(char));
+
+		if (aout == NULL)
+
+		{
+
+			free(aout);
+
+			return (NULL);
+
+		}
+
+		for (i = j = ia = 0; ia < c; j++, ia++)
+
+		{
+
+			if (av[i][j] == '\0')
+
+			{
+
+				aout[ia] = '\n';
+
+				i++;
+
+				ia++;
+
+				j = 0;
+
+			}
+
+			if (ia < c - 1)
+
+				aout[ia] = av[i][j];
+
+		}
+
+		aout[ia] = '\0';
+
+	return (aout);
+
 }
