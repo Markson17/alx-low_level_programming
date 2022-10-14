174dda468b647c272563d367413829313a05f90f
Markson Favour <95889759+Markson17@users.noreply.github.com>
2022-10-14 01:42:43 +0100

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,1 +1,13 @@
-aaa
+section     .text
+global      main
+
+main:
+mov     edx,len
+mov     ecx,msg
+mov     ebx,1
+mov     eax,4
+int     0x80
+
+section     .data
+msg     db  'Hello, Holberton',0xa
+len     equ $ - msg
